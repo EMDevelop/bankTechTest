d8297c68bf1b154b7125fb8acbcacdb8225d5259
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-27 10:45:00 +0100

    green, multiple deposits added, balance also calculated

--- spec/support/bankingAppSpec.js
+++ spec/support/bankingAppSpec.js
@@ -7,6 +7,12 @@ describe('BankAccountTransactions', () => {
   const header = 'date || credit || debit || balance\n';
   const transactionOne = '10/01/2023 || 1000.00 || || 1000.00\n';
   const singleWithdraw = '10/01/2023 || || 500.00 || -500.00\n';
+  const depositAndWithdraw =
+    '14/01/2023 || || 500.00 || 500.00\n' + transactionOne;
+  const transactionTwo = '13/01/2023 || 2000.00 || || 3000.00\n';
+  const transactionThree = '14/01/2023 || || 500.00 || 2500.00\n';
+  const twoDepositsOneWithdraw =
+    transactionThree + transactionTwo + transactionOne;
 
   it('displays the title with no transactions', () => {
     expect(app.printMyAccountStatement()).toEqual(header);
@@ -32,5 +38,49 @@ describe('BankAccountTransactions', () => {
     spyOn(app, '_createTransaction').and.returnValue(t1);
     app.makeWithdrawal();
     expect(app.printMyAccountStatement()).toBe(header + singleWithdraw);
+  });
+
+  it('deposits 1000 then withdraws 500', () => {
+    const t1 = {
+      date: '10/01/2023',
+      type: 'deposit',
+      transactionAmount: 1000.0,
+    };
+    const t2 = {
+      date: '14/01/2023',
+      type: 'withdraw',
+      transactionAmount: 500.0,
+    };
+
+    spyOn(app, '_createTransaction').and.returnValues(t1, t2);
+    app.makeDeposit();
+    app.makeWithdrawal();
+
+    expect(app.printMyAccountStatement()).toBe(header + depositAndWithdraw);
+  });
+
+  it('deposits 1000 then deposits 2000 then withdraws 500', () => {
+    const t1 = {
+      date: '10/01/2023',
+      type: 'deposit',
+      transactionAmount: 1000.0,
+    };
+    const t2 = {
+      date: '13/01/2023',
+      type: 'deposit',
+      transactionAmount: 2000.0,
+    };
+    const t3 = {
+      date: '14/01/2023',
+      type: 'withdraw',
+      transactionAmount: 500.0,
+    };
+
+    spyOn(app, '_createTransaction').and.returnValues(t1, t2, t3);
+    app.makeDeposit();
+    app.makeDeposit();
+    app.makeWithdrawal();
+
+    expect(app.printMyAccountStatement()).toBe(header + twoDepositsOneWithdraw);
   });
 });
--- src/statementReport.js
+++ src/statementReport.js
@@ -1,19 +1,54 @@
 class StatementReport {
   constructor() {
     this.header = 'date || credit || debit || balance\n';
+    this.STARTING_BALANCE = 0;
   }
 
   create(transactions) {
-    let transactionHistory = '';
-    if (transactions.length === 0) return this.header;
-    if (transactions[0].type === 'withdraw') {
-      console.log('a');
-      transactionHistory = `${transactions[0].date} || || ${transactions[0].transactionAmount}.00 || -${transactions[0].transactionAmount}.00\n`;
-    }
-    if (transactions[0].type === 'deposit') {
-      console.log('b');
-      transactionHistory = `${transactions[0].date} || ${transactions[0].transactionAmount}.00 || || ${transactions[0].transactionAmount}.00\n`;
-    }
-    return this.header + transactionHistory;
+    const header = 'date || credit || debit || balance\n';
+    const transactionSummary = this._isNoTransactions(transactions)
+      ? ''
+      : this._loopTransactions(transactions);
+    return header + transactionSummary;
+  }
+
+  _loopTransactions(transactions) {
+    let [accumulatedTransactions, currentBalance] = ['', this.STARTING_BALANCE];
+    transactions.forEach((transaction) => {
+      // calling Update New Balance
+      currentBalance = this._updateBalance(
+        currentBalance,
+        transaction.transactionAmount,
+        transaction.type
+      );
+      accumulatedTransactions = // calling print function
+        this._generatePrintStatement(transaction, currentBalance) +
+        accumulatedTransactions;
+    });
+    return accumulatedTransactions;
+  }
+
+  _updateBalance(currentBalance, transactionAmount, transactionType) {
+    return transactionType === 'deposit'
+      ? (currentBalance += transactionAmount)
+      : (currentBalance -= transactionAmount);
+  }
+
+  _generatePrintStatement(transaction, currentBalance) {
+    return transaction.type === 'deposit'
+      ? this._printDeposit(transaction, currentBalance)
+      : this._printWithdraw(transaction, currentBalance);
+  }
+
+  _printDeposit(transaction, currentBalance) {
+    return `${transaction.date} || ${transaction.transactionAmount}.00 || || ${currentBalance}.00\n`;
+  }
+
+  _printWithdraw(transaction, currentBalance) {
+    return `${transaction.date} || || ${transaction.transactionAmount}.00 || ${currentBalance}.00\n`;
+  }
+
+  _isNoTransactions(transactions) {
+    return transactions.length === 0;
   }
 }
